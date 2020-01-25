b3586567b9bb79e3f5b15879de65f3425cda24c5
hs97 <hs9@williams.edu>
2020-01-25 13:53:15 -0500

    dropdown

--- src/App.js
+++ src/App.js
@@ -1,11 +1,99 @@
 import React from 'react';
-import logo from './logo.svg';
+//import logo from './logo.svg';
 import './App.css';
-
+import Dropdown from './Components/Dropdown'
 const SHEET_ID = '1hYCZ4SXgisshcS5e-JU3NX4D9v4LOROUIb3PqUBwyiY';
 const ACCESS_TOKEN = 'ya29.Il-7B7DC4QIYx0Rexh7sTk8CW9KdqH5FLanzbNZLbwN5Gorb1O71lmTEsLMqmqB2FsmzocyW-Xou1H6S8BB2vkaRnM9mtvZdoOVqaud-Jao71FVTmnOkhd0gSrnL0q7AmQ';
 
 class App extends React.Component {
+  constructor(){
+    super()
+    this.state = {
+      HouseId: [
+        {
+          id: 0,
+          title: 'House 1',
+          selected: false,
+          key: 'HouseId'
+        },
+        {
+          id: 1,
+          title: 'House 2',
+          selected: false,
+          key: 'HouseId'
+        },
+        {
+          id: 2,
+          title: 'House 3',
+          selected: false,
+          key: 'HouseId'
+        },
+        {
+          id: 3,
+          title: 'House 4',
+          selected: false,
+          key: 'HouseId'
+        },
+        {
+          id: 4,
+          title: 'House 5',
+          selected: false,
+          key: 'HouseId'
+        },
+        {
+          id: 5,
+          title: 'House 6',
+          selected: false,
+          key: 'HouseId'
+        },
+        {
+          id: 6,
+          title: 'House 7',
+          selected: false,
+          key: 'HouseId'
+        }
+      ],
+      Task: [
+        {
+          id: 0,
+          title: 'Task 1',
+          selected: false,
+          key: 'Task'
+        },
+        {
+          id: 1,
+          title: 'Task 2',
+          selected: false,
+          key: 'Task'
+        },
+        {
+          id: 2,
+          title: 'Task 3',
+          selected: false,
+          key: 'Task'
+        }
+      ]
+    }
+  }
+
+// Changes state of dropdown when user picks
+  toggleSelected = (id, key) => {
+    let temp = JSON.parse(JSON.stringify(this.state[key]))
+    temp[id].selected = !temp[id].selected
+    this.setState({
+      [key]: temp
+    })
+  }
+// Resets the state for Dropdown Menus
+  resetThenSet = (id, key) => {
+    let temp = JSON.parse(JSON.stringify(this.state[key]))
+    temp.forEach(item => item.selected = false);
+    temp[id].selected = true;
+    this.setState({
+      [key]: temp
+    })
+  }
+
   updateSheetValues = () => {
     fetch(`https://sheets.googleapis.com/v4/spreadsheets/${SHEET_ID}:batchUpdate`, {
       method: "POST",
@@ -39,9 +127,26 @@ class App extends React.Component {
   }
   render() {
     return ( 
-      <div className = "App" >
+
+      <div className = "App">
         hello world 
+
+      
         <button onClick={this.updateSheetValues}>Submit</button>
+
+        <div className = "wrapper">
+        <Dropdown
+            title="Select HouseId"
+            list={this.state.HouseId}
+            resetThenSet={this.resetThenSet}
+        />
+
+        <Dropdown
+            title="Select Task"
+            list={this.state.Task}
+            resetThenSet={this.resetThenSet}
+        />
+      </div>
       </div>
     );
 
@@ -49,3 +154,6 @@ class App extends React.Component {
 }
 
 export default App;
+
+
+
